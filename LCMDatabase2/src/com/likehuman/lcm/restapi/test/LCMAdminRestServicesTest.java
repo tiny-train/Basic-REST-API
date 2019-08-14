c4d43e2a0690e22973d37621a7c4e49fc05554f1
Mark Davis <mark@likehuman.com>
2019-08-14 15:42:34 -0700

    initial embed of mock tomcat and supporting jar from tomcat distro

--- LCMDatabase2/src/com/likehuman/lcm/restapi/test/LCMAdminRestServicesTest.java
+++ LCMDatabase2/src/com/likehuman/lcm/restapi/test/LCMAdminRestServicesTest.java
@@ -1,20 +1,51 @@
 package com.likehuman.lcm.restapi.test;
 
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LCMAdminRestServicesTest {
-	
+
+	public static Tomcat tomcat;
+
 	@Before
 	public void setup()
 	{
 		System.err.println("Setup!");
-	}
-	
-	@Test
-	public void test()
-	{
-		assert(true);
+
+		tomcat = new Tomcat();
+
+		try {
+			
+			tomcat.addWebapp("/", "/");
+			tomcat.start();
+			
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (LifecycleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
+	@Test
+	public void test() {
+		assert (true);
+	}
+
+	@After
+	public void teardown()
+	{
+		try {
+			tomcat.stop();
+		} catch (LifecycleException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
