7208c8a6742a166bfe4f24147cdda3d494941cd8
Mark Davis <mark@likehuman.com>
2019-08-14 15:24:29 -0700

    Sample JUnit test to experiment with embedded Tomcat mocking

---
+++ LCMDatabase2/src/com/likehuman/lcm/restapi/test/LCMAdminRestServicesTest.java
@@ -0,0 +1,20 @@
+package com.likehuman.lcm.restapi.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LCMAdminRestServicesTest {
+	
+	@Before
+	public void setup()
+	{
+		System.err.println("Setup!");
+	}
+	
+	@Test
+	public void test()
+	{
+		assert(true);
+	}
+
+}
