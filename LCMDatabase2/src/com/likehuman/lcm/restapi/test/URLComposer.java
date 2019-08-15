e499bc313d9e1ceb7d418e05a8ff7c16e47d630c
Mark Davis <mark@likehuman.com>
2019-08-15 15:47:33 -0700

    added comments and spacing

--- LCMDatabase2/src/com/likehuman/lcm/restapi/test/URLComposer.java
+++ LCMDatabase2/src/com/likehuman/lcm/restapi/test/URLComposer.java
@@ -3,13 +3,26 @@ package com.likehuman.lcm.restapi.test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * The URL composer simplifies doing tests via HTTP calls by supporting defaults
+ * (localhost, port 8080, LCMDatabase) that may also be set by java properties
+ * (like -Dhostname=192.168.0.1 -Dport=8081 -Dservlet=LCM)
+ * 
+ * @author markdavis
+ *
+ */
+
 public class URLComposer {
 	
+	// append the REST function url to the path. The path should begin with /
 	public static URL composeURL(String path) throws MalformedURLException {
 		
 		String hostname = System.getProperty("hostname", "localhost");
+		
 		int port = Integer.parseInt(System.getProperty("port", "8080"));
+		
 		String servletName = System.getProperty("servlet", "LCMDatabase");
+		
 		return new URL("http://"+hostname+":"+port+"/"+servletName+path);
 		
 	}
