99524e49d3123fe580e842146926aa1b52dd8cc2
Mark Davis <mark@likehuman.com>
2019-08-15 11:36:23 -0700

    parameterized URL for test case

--- LCMDatabase2/src/com/likehuman/lcm/restapi/test/LCMAdminRestServicesTest.java
+++ LCMDatabase2/src/com/likehuman/lcm/restapi/test/LCMAdminRestServicesTest.java
@@ -1,48 +1,76 @@
 package com.likehuman.lcm.restapi.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LCMAdminRestServicesTest {
 
-	//public static Tomcat tomcat;
+	private String hostname;
+	private int port;
+	private String path;
 
 	@Before
 	public void setup()
 	{
-		System.err.println("Setup!");
-/*
-		tomcat = new Tomcat();
-
-		try {
-			
-			tomcat.addWebapp("/", "/");
-			tomcat.start();
-			
-		} catch (ServletException e) {
-			e.printStackTrace();
-		} catch (LifecycleException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-*/		
+		
+		hostname = System.getProperty("hostname", "localhost");
+		port = Integer.parseInt(System.getProperty("port", "8080"));
+		path = System.getProperty("path", "/LCMDatabase/api/lcm/admin/ping");
+		
 	}
 
 	@Test
 	public void test() {
-		assert (true);
+		
+		URL url;
+		try {
+			
+			// create url for api test
+			url = new URL("http://"+hostname+":"+port+""+path);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			
+			//specification of request type
+			con.setRequestMethod("GET");
+			
+			//assertion that the response code of the HTTP Request is 200
+			int responseCode = con.getResponseCode();
+			
+			DataInputStream wr = new DataInputStream(con.getInputStream());
+			BufferedReader br = new BufferedReader(new InputStreamReader(wr));
+			
+			String inLine = br.readLine();
+			while (inLine != null) {
+				System.err.println(inLine);
+				inLine = br.readLine();
+			}
+		
+			assertEquals(200, responseCode);	
+			
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+			assert(false);
+			
+		}
+		
 	}
 
 	@After
 	public void teardown()
 	{
-		/*
-		try {
-			tomcat.stop();
-		} catch (LifecycleException e) {
-			e.printStackTrace();
-		}*/
+		
 	}
 	
 }
